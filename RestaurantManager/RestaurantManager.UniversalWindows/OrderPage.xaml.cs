5df1b50de96af309902e91f102434cb5fbe1e0bc
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2015-09-29 22:10:46 +0100

    Module 3 Work
    Added interactive controls to add navigation to Restaurant Manager pages
    for Module 3 of edX Microsoft: DEV206.1x Introduction to XAML and
    Application Development

--- RestaurantManager/RestaurantManager.UniversalWindows/OrderPage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/OrderPage.xaml.cs
@@ -20,21 +20,16 @@ namespace RestaurantManager.UniversalWindows
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class OrderPage : Page
     {
-        public MainPage()
+        public OrderPage()
         {
             this.InitializeComponent();
         }
 
-        private void GotoOrdersView_Click(object sender, RoutedEventArgs e)
+        private void AppBarHomeButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(ExpeditePage));
-        }
-
-        private void SubmitOrder_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(OrderPage));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
